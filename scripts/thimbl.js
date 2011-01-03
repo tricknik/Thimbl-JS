2e9e1f166b73d6333e2e68c6b9780e9e354ee5db
Dmytri Kleiner <dk@telekommunisten.net>
2011-01-03 22:59:03 +0100

    added login function

--- scripts/thimbl.js
+++ scripts/thimbl.js
@@ -3,13 +3,29 @@
     this.window = window;
     this.$ = window.jQuery;
   };
-  Thimbl.prototype.profile= function(user, callback) {
-    this.window.profile = function(data, status) {
-      callback(data);
+
+  Thimbl.prototype.profile = function(user, callback) {
+    this.window._profile = function(data, status) {
+      callback(null, data);
     };
     var api_url = "http://users.thimbl.net/profile?u";
     var plan_url = [api_url, user].join("=");
-    this.$.ajax({url: plan_url, dataType: "jsonp", jsonpCallback: "profile"});
+    this.$.ajax({url: plan_url, dataType: "jsonp", jsonpCallback: "_profile"});
+  };
+  Thimbl.prototype.login = function(user, password, callback) {
+    this.window._login = function(data, status) {
+      if ("login" in data) {
+        err = null;
+        console.log("Login Succeeded");
+       } else {
+        err = Error("Login failed");
+       }
+      callback(err, data);
+    };
+    var api_url = "https://users.thimbl.net/account";
+    var login_url = [api_url,
+      [["u",user].join("="),["password",password].join("=")].join("&")].join("?");
+    this.$.ajax({url: login_url, dataType: "jsonp", jsonpCallback: "_login"});
   };
   if (typeof module !== "undefined" && module.exports) {
     module.exports.Thimbl = Thimbl;
