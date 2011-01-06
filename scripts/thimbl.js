a113c0507f57ff505b1f0f09a24db0e0b9efe0ca
Dmytri Kleiner <dk@telekommunisten.net>
2011-01-06 01:11:46 +0100

    update function in progress

--- scripts/thimbl.js
+++ scripts/thimbl.js
@@ -1,22 +1,43 @@
 (function() {
-  var Thimbl = function(window) {
-    this.window = window;
-    this.$ = window.jQuery;
+  var Thimbl = function(context) {
+    window = context;
+    $ = window.jQuery;
   };
-
+  Thimbl.prototype.update = function(user, login, property, value) {
+    var thimbl = this;
+    thimbl.profile(user, function(err, profile) {
+      if (property in {name:1,bio:1,address:1,email:1}) {
+        profile[property] = value;
+      } else {
+        profile.properties = profile.properties || {};
+        profile.properties[property] = value;
+      }  
+      window._update = function(data, status) {
+        thimbl.plan = null;
+      };
+      var plan = escape(JSON.stringify(profile));
+      var api_url = "https://users.thimbl.net/account";
+      var login_url = [api_url,
+        [["u",login].join("="),[".plan",plan].join("=")].join("&")].join("?");
+      $.ajax({url: login_url, dataType: "jsonp", jsonpCallback: "_update"});
+    });
+  };
   Thimbl.prototype.profile = function(user, callback) {
-    this.window._profile = function(data, status) {
-      callback(null, data);
-    };
-    var api_url = "http://users.thimbl.net/profile?u";
-    var plan_url = [api_url, user].join("=");
-    this.$.ajax({url: plan_url, dataType: "jsonp", jsonpCallback: "_profile"});
+    if (this.plan) callback(null, this.plan)
+    else {
+      window._profile = function(data, status) {
+        this.plan = data;
+        callback(null, data);
+      };
+      var api_url = "http://users.thimbl.net/profile?u";
+      var plan_url = [api_url, user].join("=");
+      $.ajax({url: plan_url, dataType: "jsonp", jsonpCallback: "_profile"});
+    }
   };
   Thimbl.prototype.login = function(user, password, callback) {
-    this.window._login = function(data, status) {
+    window._login = function(data, status) {
       if ("login" in data) {
         err = null;
-        console.log("Login Succeeded");
        } else {
         err = Error("Login failed");
        }
@@ -25,7 +46,7 @@
     var api_url = "https://users.thimbl.net/account";
     var login_url = [api_url,
       [["u",user].join("="),["password",password].join("=")].join("&")].join("?");
-    this.$.ajax({url: login_url, dataType: "jsonp", jsonpCallback: "_login"});
+    $.ajax({url: login_url, dataType: "jsonp", jsonpCallback: "_login"});
   };
   if (typeof module !== "undefined" && module.exports) {
     module.exports.Thimbl = Thimbl;
